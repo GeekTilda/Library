7aec860af770147d670e42b37ff23eca7bc07d88
Tilda Lindblad Aderud <lindbladaderud.tilda@elev.ga.ntig.se>
2023-04-19 13:22:39 +0200

    Experimenting with files and different ways to split and trim them.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -13,22 +14,44 @@ public class Main {
             System.out.println("Didnt work");
         }
         FileWriter writer = new FileWriter(server, true);
-        writer.write("Hej");
-        writer.write("då");
+        //writer.write("Hej");
+        //writer.write("då");
+
+
+        Book book = new Book(new Author("Anna",47),"52 Fjärilar",52,new Date());
+        book.addGenre(Genre.HORROR);
+        book.addGenre(Genre.COMEDY);
+        //writer.write("\n" + "Name:" + book.getName() + " Pages:" + book.getPages() + " Genres:" + book.getGenres() + " Author:" + book.getAuthor() + " Date:" + book.getDate() + " Borrowed:" + book.getBorrowed());
         writer.close();
         Scanner scanner = new Scanner(server);
+        ArrayList<String> text = new ArrayList<String>();
+        String vadsomhelst = scanner.nextLine();
         while (scanner.hasNextLine()) {
             String hej = scanner.next();
-            System.out.println(hej);
+            text.add(hej);
+            //System.out.println(hej);
+        }
+        String[] seperator = vadsomhelst.trim().split("[.]");
+        for (String i : seperator) {
+            System.out.println(i);
         }
     }
-    public Boolean fileExist(File server) {
-        if (server.exists()) {
+    public Boolean fileExist(File customers, File books) {
+        if (customers.exists() && books.exists()) {
             return true;
         }
-        else {
-            File file = new File("Server.txt");
+        else if (customers.exists() && !books.exists()){
+            File file = new File("Books.txt");
+            return false;
+        } else if (!customers.exists() && books.exists()) {
+            File file = new File("Customers.txt");
+            return false;
+        } else {
+            File file = new File("Books.txt");
+            File file2 = new File("Customers.txt");
             return false;
         }
     }
+
+
 }
